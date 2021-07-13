5223c9ef0777d25fd16ecb60fc165437a164799e
mushi42 <musharaf42@gmail.com>
2021-07-13 23:45:59 +0500

    asd

--- router/index.js
+++ router/index.js
@@ -4,6 +4,7 @@ const usersRoutes = require('./users.routes');
 const ticketsRoutes = require('./tickets.routes');
 const univeristyRoutes = require('./university.routes');
 const uploadRoutes = require('./upload.routes');
+const mailRoutes = require('./mail.routes');
 
 
 const router = express.Router();
@@ -12,6 +13,7 @@ router.use('/users', usersRoutes);
 router.use('/tickets', ticketsRoutes);
 router.use('/university', univeristyRoutes);
 router.use('/uploads', uploadRoutes);
+router.use('/contactMail', mailRoutes);
 
 
 
