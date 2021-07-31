6b3c23b9bb6d3b390a4c617abe33d0b4f83c1070
aqibijaz <45151704+aqibijaz@users.noreply.github.com>
2021-07-31 16:15:11 -0700

    Emial

--- router/mail.routes.js
+++ router/mail.routes.js
@@ -62,5 +62,38 @@ router.post("/counselling_session", protectRoutes, async (req, res) => {
 
     res.send("Email Sent")
 });
+router.post("/preference", protectRoutes, async (req, res) => {
+
+    const { user, body } = req
+
+    console.log('body', String(body.univerityList))
+
+    const userFound = await User.findOne({ _id: user.userId})
+
+    if (!userFound) res.sendStatus(401)
+    let testAccount = await nodemailer.createTestAccount();
+
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'drivethru.pk@gmail.com',
+            pass: 'aorntzlgmzhyojuw',
+        },
+    });
+
+
+    let info = await transporter.sendMail({
+        from: 'drivethru.pk@gmail.com',
+        to: 'aqibijaz3@gmail.com',
+        subject: "Student University Preference",
+        text: `Email : ${userFound.email}\nUniversity Preference ? : ${String(body.univerityList)}`
+    });
+
+    res.send("Email Sent")
+});
 
 module.exports = router
